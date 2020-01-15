9cbee0810bcee2fa6ebe6d6aaa48d193b1a0cf5d
bigmac <electmattter@gmail.com>
2020-01-15 14:54:52 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/werun/rezu/com/we_are_running/MainActivity.kt
@@ -0,0 +1,12 @@
+package werun.rezu.com.we_are_running
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
